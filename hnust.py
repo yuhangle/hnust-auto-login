cd659303423486fd2489b8b001317729f123856a
yuhang2006 <88643109+yuhangle@users.noreply.github.com>
2024-05-15 11:50:32 +0800

    Add files via upload

--- hnust.py
+++ hnust.py
@@ -6,7 +6,7 @@ from os.path import exists
 from time import localtime, sleep
 from types import MethodType
 import json
-
+import time
 import click
 import socket
 import requests
@@ -168,8 +168,9 @@ def login(username, password, operator):
     """
     # 如果网络可以访问则直接退出程序
     if isInternetAccess():
-        logger.info('网络可以访问, 退出程序')
-        return
+        logger.info('网络可以访问')
+        print("5秒后开始循环自动登录")
+        time.sleep(5)
 
         # 因为密码是不能给别人看见的，所有要在这里检测缓存的密码
 
@@ -233,14 +234,14 @@ def login(username, password, operator):
             message = "找不到主机(你可能没有连接校园网wifi)"
             logger.error(message)
         except BaseException as e:
-            logger.error("未知错误[{}]".format(type(e)))
+            logger.error("未知错误（大概率是因为已经登录校园网导致的登录网页报错，请无视）[{}]".format(type(e)))
             if resp != '':
                 logger.error("resp.text: {}".format(resp.text))
         finally:
             retry += 1
-            sleep(1)
-            if retry >= 600:
-                break
+            sleep(5)
+ #           if retry >= 600:
+ #               break
             # logger.info("尝试次数: " + str(retry))
 
 
@@ -248,11 +249,12 @@ def login(username, password, operator):
 @click.command()
 def getInfo():
     """
-    【没写】
+    【这个功能还没写】
     返回网页包含了用户网络信息，使用re库来获取script标签里面的变量
     """
     resp = get("http://login.hnust.cn/?isReback=1")
     print(resp.text)
+    print("都说了这个功能还没写")
 
 
 def _logOut():
@@ -290,36 +292,45 @@ def logOut():
 @click.command()
 def addStartup():
     """把这个程序添加到开机启动里面 这样每次开机就能够自动检测登录了"""
+    print("已经禁用此功能, 若要实现开机自启请使用windows计划任务或者创建快捷方式复制到自启动目录代替之")
+    ifop = input("是否打开Windows自启动目录[Y,n]") or "Y"
 
-    try:
+    if ifop == "Y":
         os.popen(
             "explorer.exe \"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp")
-        logger.info("记得使用管理员的身份运行")
-        logger.warning("已经禁用此功能, 若要实现开机自启请使用windows计划任务代替之")
-        cmd1 = r"copy hnust.py \"C:\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\StartUp\\hnust.py\""
-        cmd2 = r"copy .config \"C:\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\StartUp\\.config\""
-        cmd3 = r"copy hnust.exe \"C:\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\StartUp\\hnust.exe\""
-
-        logger.info("cmd1: " + cmd1)
-        logger.info("cmd2: " + cmd2)
-        logger.info("cmd3: " + cmd3)
-
-        result1 = os.popen(
-            cmd1).read()
-        result2 = os.popen(
-            cmd2).read()
-        result3 = os.popen(
-            cmd3).read()
-        logger.info(result1)
-        logger.info(result2)
-        logger.info(result3)
-
-    except IOError as e:
-        logger.error("Unable to copy file. %s" % e)
-        raise
-    except BaseException:
-        logger.error("Unexpected error:", sys.exc_info())
-        raise
+        print("已为您打开Windows自启动文件夹，请创建程序快捷方式并复制到自启动文件夹")
+        
+    else:
+        return
+#    try:
+#        os.popen(
+#            "explorer.exe \"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp")
+#        logger.info("记得使用管理员的身份运行")
+#        logger.warning("已经禁用此功能, 若要实现开机自启请使用windows计划任务代替之")
+#        cmd1 = r"copy hnust.py \"C:\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\StartUp\\hnust.py\""
+#        cmd2 = r"copy .config \"C:\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\StartUp\\.config\""
+#        cmd3 = r"copy hnust.exe \"C:\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\StartUp\\hnust.exe\""
+#
+#        logger.info("cmd1: " + cmd1)
+#       logger.info("cmd2: " + cmd2)
+#        logger.info("cmd3: " + cmd3)
+#
+#        result1 = os.popen(
+#            cmd1).read()
+#        result2 = os.popen(
+#            cmd2).read()
+#        result3 = os.popen(
+#            cmd3).read()
+#        logger.info(result1)
+#        logger.info(result2)
+#        logger.info(result3)
+#
+#    except IOError as e:
+#        logger.error("Unable to copy file. %s" % e)
+#        raise
+#    except BaseException:
+#        logger.error("Unexpected error:", sys.exc_info())
+#        raise
 
 
 #自动登录校园网功能（连接路由器版本）
@@ -333,14 +344,15 @@ def addStartup():
 
 @click.option("--rip", '-i', prompt="你的路由器IP地址", default=getProperties("rip"))
 
-def router(username, password, operator,rip):
+def router(username, password, operator,rip,):
     """
     login命令的路由器网络版本，填写路由器ip地址使得路由器可登录校园网
     """
     # 如果网络可以访问则直接退出程序
     if isInternetAccess():
-        logger.info('网络可以访问, 退出程序')
-        return
+        logger.info('网络可以访问')
+        print("5秒后开始循环自动登录")
+        time.sleep(5)
 
         # 因为密码是不能给别人看见的，所有要在这里检测缓存的密码
 
@@ -405,25 +417,56 @@ def router(username, password, operator,rip):
             message = "找不到主机(你可能没有连接校园网wifi)"
             logger.error(message)
         except BaseException as e:
-            logger.error("未知错误[{}]".format(type(e)))
+            logger.error("未知错误（大概率是因为已经登录校园网导致的登录网页报错，请无视）[{}]".format(type(e)))
             if resp != '':
                 logger.error("resp.text: {}".format(resp.text))
         finally:
             retry += 1
-            sleep(1)
+            sleep(5)
             # logger.info("尝试次数: " + str(retry))
 
+# noinspection PyBroadException
+@click.command()
+def autologin():
+    """设置默认登录选项，请确保程序已记录您的配置信息，否则自动登录无效"""
+    mode = input("请选择默认自动登录模式[login,router]:")
+
+    if mode == "login":
+        with open(".autologin","w") as file:
+            pass
+        print("已开启默认直连模式自动登录，以后启动程序将默认采用直连模式自动登录，若要关闭请删除工作目录下.autologin文件")
+
+    elif mode == "router":
+        with open(".router","w") as file:
+            pass
+        print("已开启默认路由器网络模式自动登录，以后启动程序将默认采用路由器网络模式自动登录，若要关闭请删除工作目录下.router文件")
+
+    else:
+        print("无效的选择，请输入login或router")
+        return
 
 cli.add_command(login)
 cli.add_command(logOut)
 cli.add_command(getInfo)
 cli.add_command(addStartup)
 cli.add_command(router)
+cli.add_command(autologin)
 
 init_logger(level=logging.DEBUG)
 if __name__ == '__main__':
-    if "StartUp" in sys.argv[0] and len(sys.argv) <= 1:  # 如果在启动目录下则自动检测登录
-        sys.argv = [sys.argv[0], "login",
+    wdir = os.getcwd()
+    dir_name = os.path.basename(wdir)
+    if os.path.exists(".autologin") or os.path.exists(".router"):  # 检测自动登录标记文件则自动登录
+        if os.path.exists(".autologin"):
+            sys.argv = [sys.argv[0], "login",
+                    "--username", getProperties(
+                        "username") if getProperties("username") else None,
+                    "--password", getProperties(
+                        "password") if getProperties("password") else None,
+                    "--operator", getProperties(
+                        "operator") if getProperties("operator") else None,]
+        else:
+            sys.argv = [sys.argv[0], "router",
                     "--username", getProperties(
                         "username") if getProperties("username") else None,
                     "--password", getProperties(
@@ -434,4 +477,22 @@ if __name__ == '__main__':
                         "rip") if getProperties("rip") else None,
                     ]
     cli()
+
+#init_logger(level=logging.DEBUG)
+#if __name__ == '__main__':
+  #  if "StartUp" in sys.argv[0] and len(sys.argv) <= 1:  # 如果在启动目录下则自动检测登录
+   #     if os.path.exists(".router"):
+    #        sys.argv = [sys.argv[0], "router",]
+     #   else:
+      #      sys.argv = [sys.argv[0], "login",
+       #             "--username", getProperties(
+        #                "username") if getProperties("username") else None,
+         #           "--password", getProperties(
+          #              "password") if getProperties("password") else None,
+           #         "--operator", getProperties(
+            #            "operator") if getProperties("operator") else None,
+             #       "--rip", getProperties(
+              #          "rip") if getProperties("rip") else None,
+               #     ]
+#    cli()
 #此版本为Notype学长的hnust-auto-login的改进版，原版无法在路由器环境下工作，故我基于原版新增了路由器网络功能，将login命令中的自动获取设备ip改成手动填写自己的路由器ip，使得软件可在路由器环境下自动登录校园网。
